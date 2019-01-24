5607898a59b4f529a67b29411d49aa6042ba1f51
zpsample <46982857+zpsample@users.noreply.github.com>
2019-01-24 10:19:43 +0800

    添加启动类的注释
    添加启动类的注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// 测试demo，启动入口
 @SpringBootApplication
 public class DemoApplication {
 
